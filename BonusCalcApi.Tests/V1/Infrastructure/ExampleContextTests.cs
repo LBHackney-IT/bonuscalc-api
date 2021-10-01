f2ec16c672535d9bf1ae1c01b4023089a1072e43
Dave Dean <david.dean@hackney.gov.uk>
2021-10-01 17:19:08 +0100

    dotnet format

--- BonusCalcApi.Tests/V1/Infrastructure/ExampleContextTests.cs
+++ BonusCalcApi.Tests/V1/Infrastructure/ExampleContextTests.cs
@@ -8,17 +8,7 @@ namespace BonusCalcApi.Tests.V1.Infrastructure
     [TestFixture]
     public class DatabaseContextTest : DatabaseTests
     {
-        [Test]
-        public void CanGetADatabaseEntity()
-        {
-            //    var databaseEntity = DatabaseEntityHelper.CreateDatabaseEntity();
 
-            //    DatabaseContext.Add(databaseEntity);
-            //    DatabaseContext.SaveChanges();
-
-            //    var result = DatabaseContext.DatabaseEntities.ToList().FirstOrDefault();
-
-            //    Assert.AreEqual(result, databaseEntity);
-            //}
-        }
     }
+
+}
