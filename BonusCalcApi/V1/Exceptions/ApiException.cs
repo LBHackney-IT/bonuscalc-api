7aab311728f17187d7a3fe4b60c95c557f56eb33
Dave Dean <david.dean@hackney.gov.uk>
2021-10-01 16:25:09 +0100

    Add exception

---
+++ BonusCalcApi/V1/Exceptions/ApiException.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BonusCalcApi.V1.Exceptions
+{
+    public class ApiException : Exception
+    {
+        public int StatusCode { get; }
+
+        public ApiException(HttpStatusCode statusCode, string message)
+            : this((int) statusCode, message)
+        {
+        }
+
+        public ApiException(int statusCode, string message)
+            : this(message)
+        {
+            this.StatusCode = (int) statusCode;
+        }
+
+        public ApiException(string message) : base(message) { }
+
+        public ApiException() { }
+
+        public ApiException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
