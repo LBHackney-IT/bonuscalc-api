2c6241eb162ebdf6d7f2f827e7ed8aa8906206d4
Andrew White <andrew.white@unboxed.co>
2021-10-12 11:20:28 +0100

    Add missing annotations and model snapshot

--- BonusCalcApi/V1/Infrastructure/Operative.cs
+++ BonusCalcApi/V1/Infrastructure/Operative.cs
@@ -4,13 +4,30 @@ namespace BonusCalcApi.V1.Infrastructure
 {
     public class Operative
     {
-        [Key] public string Id { get; set; }
+        [Key]
+        [StringLength(6)]
+        public string Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string Name { get; set; }
+
+        [Required]
+        [StringLength(10)]
         public string Trade { get; set; }
+
+        [Required]
+        [StringLength(10)]
         public string Section { get; set; }
+
+        [Required]
+        [StringLength(10)]
         public string Scheme { get; set; }
+
         public int SalaryBand { get; set; }
+
         public bool FixedBand { get; set; }
+
         public bool IsArchived { get; set; }
     }
 }
