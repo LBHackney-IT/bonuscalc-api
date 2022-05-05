85bd6a4c89841aa9592f872a148fb51e0a3e7136
Andrew White <andrew.white@unboxed.co>
2022-05-05 13:42:51 +0100

    Implement ISerializable for the test exception

--- BonusCalcApi/V1/UseCase/ThrowOpsErrorUsecase.cs
+++ BonusCalcApi/V1/UseCase/ThrowOpsErrorUsecase.cs
@@ -4,7 +4,7 @@ namespace BonusCalcApi.V1.UseCase
     {
         public static void Execute()
         {
-            throw new TestOpsErrorException();
+            throw new TestOpsErrorException("This is a test exception to test our integrations");
         }
     }
 }
