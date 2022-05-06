86d3f35d12b9209d9bfb2ea8aa83844cf57da4e3
Andrew White <andrew.white@unboxed.co>
2022-05-06 16:00:24 +0100

    Tag branch/pull request in SonarCloud

--- ci.sh
+++ ci.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 
-dotnet sonarscanner begin \
-    /k:"LBHackney-IT_bonuscalc-api" \
-    /o:"lbhackney-it" \
-    /d:sonar.host.url=https://sonarcloud.io \
-    /d:sonar.cs.opencover.reportsPaths=coverage.xml \
-    /d:sonar.login="${SONAR_TOKEN}"
+args=(
+  /k:LBHackney-IT_bonuscalc-api
+  /o:lbhackney-it
+  /d:sonar.host.url=https://sonarcloud.io
+  /d:sonar.cs.opencover.reportsPaths=coverage.xml
+  /d:sonar.login="${SONAR_TOKEN}"
+)
+
+if [ "$CIRCLE_PULL_REQUEST" != "" ]; then
+  args+=(/d:sonar.pullrequest.key="$(basename $CIRCLE_PULL_REQUEST)")
+  args+=(/d:sonar.pullrequest.branch="${CIRCLE_BRANCH}")
+elif [ "$CIRCLE_BRANCH" != "" ]; then
+  args+=(/d:sonar.branch.name="${CIRCLE_BRANCH}")
+fi
+
+dotnet sonarscanner begin "${args[@]}"
 
 dotnet build --no-incremental
 
 coverlet ./BonusCalcApi.Tests/bin/Debug/netcoreapp3.1/BonusCalcApi.Tests.dll \
-    --target "dotnet" \
-    --targetargs "test --no-build" \
-    -f=opencover \
-    -o="coverage.xml"
+  --target "dotnet" \
+  --targetargs "test --no-build" \
+  -f=opencover \
+  -o="coverage.xml"
 
 status=$?
 
 dotnet sonarscanner end \
-    /d:sonar.login="${SONAR_TOKEN}"
+  /d:sonar.login="${SONAR_TOKEN}"
 
 exit $status
