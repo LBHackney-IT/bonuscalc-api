b8cf3aceb47933a97e03717828b4ec0442f9f00c
Dave Dean <david.dean@hackney.gov.uk>
2022-10-18 11:55:40 +0100

    Add stored proc for archiving operatives

---
+++ BonusCalcApi/V1/Infrastructure/Migrations/20221018102613_AddArchiveOperativeSp.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace V1.Infrastructure.Migrations
+{
+    public partial class AddArchiveOperativeSp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+CREATE OR REPLACE PROCEDURE public.sp_archive_operative(op_id text)
+LANGUAGE SQL
+AS $BODY$
+UPDATE public.operatives SET is_archived = true WHERE id = op_id
+$BODY$;
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+DROP PROCEDURE IF EXISTS public.sp_archive_operative(op_id text);
+            ");
+        }
+    }
+}
