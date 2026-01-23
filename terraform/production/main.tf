10215dd53aa99740ca87193d4d811baae28e266d
C̵͉͋̔͞ā̤̓̍͘l̙͖̑̾ͣl̙͖̑̾ͣư̡͕̭̇ḿ̬̏ͤͅ M͉̅ͮ͒ͤā̤̓̍͘c͕͗ͤ̕̕p̞̈͑̚͞ḣ̖̻͛̓ẹ̿͋̒̕r̴̨̦͕̝s̠҉͍͊ͅo̯̱̊͊͢ṇ̤͛̒̍ <callum.macpherson@hackney.gov.uk>
2026-01-23 09:48:02 +0000

    Update snapshot identifier

--- terraform/production/main.tf
+++ terraform/production/main.tf
@@ -78,7 +78,7 @@ module "database" {
   publicly_accessible = false
   project_name = "bonus calc"
 
-  snapshot_identifier = "awsbackup:job-dfd41b58-ea1c-2e88-0b34-53163122fe43"
+  snapshot_identifier = "awsbackup:copyjob-eb1f387a-6eab-43e6-834c-4d271fb57eae"
 
   additional_tags = {
     BackupPolicy = "Prod"
